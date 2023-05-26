e8dbfdf9a6932f6f747609230e85aa0bafb6ef07
Bruno Pimentel <bpimente@redhat.com>
2023-05-26 10:18:02 -0300

    Fixes deduplication
    Some changes in the PURLs generated by Syft was interfering with the
    deduplication.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- merge.py
+++ merge.py
@@ -1,14 +1,15 @@
 # Merges Cachi2 reported components into a RHTAP/Syft SBOM
 import json
+from urllib.parse import urlsplit, quote_plus
 
-with open("cachi2.bom.json") as file:
+with open("gomod-pandemonium/cachi2.bom.json") as file:
     cachi2_sbom = json.load(file)
 
-with open("rhtap.bom.json") as file:
+with open("gomod-pandemonium/rhtap.bom.json") as file:
     rhtap_sbom = json.load(file)
 
-# we want to remove local Golang modules since they'll
-# be reported differently by Cachi2
+# we want to remove locally replaced Golang modules
+# since they'll be reported differently by Cachi2
 def is_local_golang_module(component: dict) -> bool:
     return (
         component.get("purl", "").startswith("pkg:golang") and
@@ -16,9 +17,18 @@ def is_local_golang_module(component: dict) -> bool:
     )
 
 
-def unique_key(component: dict) -> str:
-    if component.get("purl", ""):
-        return component.get("purl")
+def unique_key_cachi2(component: dict) -> str:
+    url = urlsplit(component["purl"])
+    # omit the query part, since Cachi2 sets it and Syft does not
+    return url.scheme + url.path
+
+
+def unique_key_rhtap(component: dict) -> str:
+    # very few components don't have a purl, such as type OS
+    if "purl" in component.keys():
+        parts = component["purl"].split("@")
+        # Syft does not encode special characters in the version
+        return parts[0] + quote_plus(parts[1])
 
     return component.get('name', '') + '@' + component.get('version', '')
 
@@ -29,7 +39,7 @@ filtered_rhtap_components = [
 ]
 
 indexed_components = {
-    unique_key(component): component
+    unique_key_cachi2(component): component
     for component in cachi2_sbom["components"]
 }
 
@@ -39,7 +49,7 @@ for component in indexed_components.values():
 
 # adds non-duplicate RHTAP components into Cachi2's component list
 for component in filtered_rhtap_components:
-    key = unique_key(component) 
+    key = unique_key_rhtap(component)
     if key not in indexed_components.keys():
         indexed_components[key] = component
 
